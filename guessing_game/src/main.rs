76f0ff3a1761d1f40ff87bffb39bdf360f03c3d0
VladimirKataev <vova.kataev@gmail.com>
2025-10-31 15:20:33 -0700

    MVP guessing Game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,18 +4,31 @@ use std::cmp::Ordering;
 
 fn main() {
 
-    let mut guess = String::new(); // mutable string
     let secret = rand::thread_rng().gen_range(1..=100);
     println!("Guess the number (Not {}):", secret);
 
-    io::stdin()
-        .read_line(&mut guess) // read into a mutable reference
-        .expect("Failed to read line"); // deal with result potentially being an Err
-    
-    println!("You guessed {guess}");
-    match guess.cmp(&secret){
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("Perfect"),
+
+    loop{
+        let mut guess = String::new(); // mutable string
+        io::stdin()
+            .read_line(&mut guess) // read into a mutable reference
+            .expect("Failed to read line"); // deal with result potentially being an Err
+
+        let guess : u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("invalid");
+                continue;
+            },
+        };
+        println!("You guessed {guess}");
+        match guess.cmp(&secret){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Perfect");
+                break;
+            },
+        }
     }
 }

8fe1d9f88b13a6a38161cae8d9b0c1537850081e
VladimirKataev <vova.kataev@gmail.com>
2025-11-02 11:02:28 -0800

    3.2

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let x = 5;
 
@@ -16,5 +18,37 @@ fn main() {
 
     // let mut spaces = "   ";
     // spaces = spaces.len(); // CAN NOT change an existing variable's type though
+
+
+    // types:
+    // u32, i64, char, bool, isize & usize, etc for primitives (no doubles?) 
+    // overflows are checked for on debug builds, and there are workarounds for releases
+    // (i64, bool, char) for tuples
+    
+
+    // arrays can be done using the following:
+    let mut a : [i32; 5] = [1,2,3,4,5];
+    a[3] = -1;
+    let a = [3 ; 5]; // for initial creation
+
+    // out of bounds are checked for at compile: 
+    // println!("{}", a[5]); // causes a crash
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 
 }

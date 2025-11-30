5f97be70bc0ab3beeeef3d74c59399af5287d278
VladimirKataev <vova.kataev@gmail.com>
2025-11-30 14:47:47 -0800

    debug based struct printing

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
     let rect1 = rectangle{length : 50, width : 30};
-    println!("Area = {}", area(rect1));
+    println!("Area = {}", area(&rect1));
+    println!("Rect = {rect1:?}");
 }
-
+#[derive(Debug)]
 struct rectangle{
     length : u32,
     width : u32,
 }
 
-fn area(dimensions: rectangle) -> u32 {
+fn area(dimensions: &rectangle) -> u32 {
     dimensions.length * dimensions.width
 }

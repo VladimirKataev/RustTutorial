3fc429275468fb0a2d4e69fd590c921a03e61a0e
VladimirKataev <vova.kataev@gmail.com>
2025-11-30 14:44:37 -0800

    undid duplicate commit

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,16 +1,13 @@
 fn main() {
-    let rect1 = Rectangle{
-        length : 50, 
-        width : 30};
-    println!("Area = {}", area(&rect1));
-    println!("Rectangle: {}", rect1);
+    let rect1 = rectangle{length : 50, width : 30};
+    println!("Area = {}", area(rect1));
 }
 
-struct Rectangle{
+struct rectangle{
     length : u32,
     width : u32,
 }
 
-fn area(dimensions: &Rectangle) -> u32 {
+fn area(dimensions: rectangle) -> u32 {
     dimensions.length * dimensions.width
 }

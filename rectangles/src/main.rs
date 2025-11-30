5158d1d2d96225be55cd21bfd27931fc11bf2862
VladimirKataev <vova.kataev@gmail.com>
2025-11-30 14:51:56 -0800

    Started 5.3. Moved class and Capitalized correctly

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,15 +1,19 @@
-fn main() {
-    let rect1 = rectangle{length : 50, width : 30};
-    println!("Area = {}", area(&rect1));
-    println!("Rect = {rect1:?}");
-    dbg!(&rect1);
-}
 #[derive(Debug)]
-struct rectangle{
+struct Rectangle{
     length : u32,
     width : u32,
 }
 
-fn area(dimensions: &rectangle) -> u32 {
+
+
+
+fn main() {
+    let rect1 = Rectangle{length : 50, width : 30};
+    println!("Area = {}", area(&rect1));
+    println!("Rect = {rect1:?}");
+    dbg!(&rect1);
+}
+
+fn area(dimensions: &Rectangle) -> u32 {
     dimensions.length * dimensions.width
 }

451bda5d1cc9df26898ad94e7f09872a87cd5624
VladimirKataev <vova.kataev@gmail.com>
2025-11-30 14:41:05 -0800

    reference based optimization

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    let rect1 = Rectangle{length : 50, width : 30};
+    let rect1 = Rectangle{
+        length : 50, 
+        width : 30};
     println!("Area = {}", area(&rect1));
+    println!("Rectangle: {}", rect1);
 }
 
 struct Rectangle{

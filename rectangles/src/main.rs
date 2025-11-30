fddd83291a69c660d873307183e3001956e8f4b8
VladimirKataev <vova.kataev@gmail.com>
2025-11-30 15:00:34 -0800

    naming conflicts between vars and methods are resolved by brackets

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -7,6 +7,9 @@ impl Rectangle{
     fn area(&self) -> u32 {
         self.length * self.width
     }
+    fn width(&self) ->bool{
+        self.width > 0
+    }
 }
 
 
@@ -15,6 +18,7 @@ fn main() {
     let rect1 = Rectangle{length : 50, width : 30};
     println!("Area = {}", rect1.area());
     println!("Rect = {rect1:?}");
+    println!("is width above 0? {}", rect1.width());
     dbg!(&rect1);
 }
 

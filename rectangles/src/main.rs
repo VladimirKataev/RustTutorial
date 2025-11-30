7b23e6a294af4dde2b1917ab5e48aa53099e9b04
VladimirKataev <vova.kataev@gmail.com>
2025-11-30 15:31:28 -0800

    Constructors, multiple impls are possible

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -15,6 +15,12 @@ impl Rectangle{
         ((self.length > other.width) && (self.width > other.length)) // rotate 90deg
 
     }
+    fn constructSquare(size : u32) -> Self{
+        Self{
+            width : size,
+            length : size,
+        }
+    }
 }
 
 
@@ -23,8 +29,8 @@ fn main() {
     let rect1 = Rectangle{length : 50, width : 30};
     let rect2 = Rectangle{length : 60, width : 40};
     let rect3 = Rectangle{length : 30, width : 30};
-
-    println!("Rectangles are {rect1:?}, {rect2:?}, {rect3:?}");
+    let sq = Rectangle::constructSquare(20);
+    println!("Rectangles are:\n\t{rect1:?},\n\t{rect2:?},\n\t{rect3:?},\n\t{sq:?}");
     println!("Can rect1 hold rect2? {}", rect1.canHold(&rect2));
     println!("Can rect2 hold rect1? {}", rect2.canHold(&rect1));
 }

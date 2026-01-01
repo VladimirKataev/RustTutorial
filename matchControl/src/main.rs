ba67526dc18b91ce7bec918cc03c5272f7fdd3d1
VladimirKataev <vova.kataev@gmail.com>
2026-01-01 11:12:39 -0800

    enum method for state validity, function for descriptions

--- matchControl/src/main.rs
+++ matchControl/src/main.rs
@@ -25,6 +25,15 @@ fn main() {
         _ => (), // for a do-nothing operation
     }
 
+    let coin = Coin::Quarter(UsState::Alaska);
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {state:?}!");
+    } else {
+        count += 1;
+    }
+
 
 }
 
@@ -41,6 +50,30 @@ enum UsState {
     Alaska,
     // --snip--
 }
+
+impl UsState {
+    fn existed_in(&self, year: u16) -> bool {
+        match self {
+            UsState::Alabama => year >= 1819,
+            UsState::Alaska => year >= 1959,
+            // -- snip --
+        }
+    }
+}
+
+fn describe_state_quarter(coin: Coin) -> Option<String> {
+    if let Coin::Quarter(state) = coin {
+        if state.existed_in(1900) {
+            Some(format!("{state:?} is pretty old, for America!"))
+        } else {
+            Some(format!("{state:?} is relatively new."))
+        }
+    } else {
+        None
+    }
+}
+
+
 
 enum Coin {
     Penny,

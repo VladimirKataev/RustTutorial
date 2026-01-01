f4be34786a81dc7cea8a85c603d9a7093ec6a654
VladimirKataev <vova.kataev@gmail.com>
2026-01-01 11:23:52 -0800

    if let demonstration

--- matchControl/src/main.rs
+++ matchControl/src/main.rs
@@ -40,6 +40,19 @@ fn main() {
 
 }
 
+// Two identical implementations
+
+// let config_max = Some(3u8);
+// match config_max {
+//     Some(max) => println!("The maximum is configured to be {max}"),
+//     _ => (),
+// }
+
+// let config_max = Some(3u8);
+// if let Some(max) = config_max {
+//     println!("The maximum is configured to be {max}");
+// }
+
 
 
 fn add_fancy_hat() {}

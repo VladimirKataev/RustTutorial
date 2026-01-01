aca43ba1315c58f29957fd0840c1c93c0a8ac351
VladimirKataev <vova.kataev@gmail.com>
2026-01-01 11:00:30 -0800

    enum variants can have arguments

--- matchControl/src/main.rs
+++ matchControl/src/main.rs
@@ -1,16 +1,25 @@
 fn main() {
     let money = Coin::Penny;
     let value = value_in_cents(money);
+    let money = Coin::Quarter(UsState::Alaska);
+    let value = value_in_cents(money);
     println!("{value}");
 }
 
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState), // can have arguments in enum types
 }
+
 
 fn value_in_cents(coin: Coin) -> u8 {
     match coin {
@@ -20,6 +29,8 @@ fn value_in_cents(coin: Coin) -> u8 {
         },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("Quarter from {state:?}");
+            25},
     }
 }

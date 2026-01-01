bb3f82b96ee3e82b59b93a59cc16bf8dec4cb2ac
VladimirKataev <vova.kataev@gmail.com>
2026-01-01 10:24:44 -0800

    struct-based address implementation

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,32 @@
+enum IpAddrKind{
+    V4,
+    V6
+}
+
+fn route(ip_kind: IpAddrKind) {}
+
+
+
+struct IpAddr{
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Enums!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    route(four);
+    route(six);
+
+    let home = IpAddr{
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    
+    let loopback = IpAddr{
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
 }
